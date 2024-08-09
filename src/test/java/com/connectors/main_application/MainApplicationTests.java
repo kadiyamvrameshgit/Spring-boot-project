6cb2167167316ff1398b73f0fc847f361c81e648
kadiyam Venkata Ramesh <kadiyamvramesh@gmail.com>
2024-08-08 22:19:28 -0700

    Intial Commit

---
+++ src/test/java/com/connectors/main_application/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectors.main_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
